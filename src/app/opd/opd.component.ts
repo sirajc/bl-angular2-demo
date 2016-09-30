26ade505b3ded8bbeff5e3943cb3e869666c97cb
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-10-01 04:14:15 +0530

    feat: add doctors

--- src/app/opd/opd.component.ts
+++ src/app/opd/opd.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bl-opd',
-  template: 'Welcome to BL Opd'
+  templateUrl: './opd.component.html',
+  styleUrls: [ './opd.component.scss' ]
 })
-export class OpdComponent { }
+export class OpdComponent {
+  name: string = 'BL OPD';
+
+  doctors = [
+    'Tasleem',
+    'Bhanushali',
+    'Tanveer',
+    'Chandra',
+    'Gala'
+  ];
+}
--- src/app/opd/opd.module.ts
+++ src/app/opd/opd.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { OpdComponent } from './opd.component';
 
 @NgModule({
-  imports: [],
+  imports: [ CommonModule ],
   exports: [ OpdComponent ],
   declarations: [ OpdComponent ]
 })
